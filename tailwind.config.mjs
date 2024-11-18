a79ec1a1f478e2677b9cbdd0939dd8c8ad1d8e61
Ana <91411170+AnaCascante@users.noreply.github.com>
2024-11-18 06:05:09 +0100

    fixed tailwind configuration

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-    "./src/**/*.{html,js,css}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx,mjs}',
+    './src/**/*.{html,js,css}',
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
